26783b2d436303ba472ca56c2c8cdd19e1c31c66
William Da Silva Vieira <89865877+WilliamDaSilvaVieira@users.noreply.github.com>
2023-10-25 03:00:11 -0300

    Create configuration.nix

---
+++ configuration.nix
@@ -0,0 +1,580 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running `nixos-help`).
+
+{ config, pkgs, inputs, lib, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      <home-manager/nixos>
+    ];
+
+  # Boot.
+  boot = {
+    kernelPackages = pkgs.linuxPackages_latest;
+    kernelParams = [ "nvidia_drm.modeset=1" ];
+    kernelModules = [
+      "nvidia"
+      "nvidia_modeset"
+      "nvidia_uvm"
+      "nvidia_drm"
+    ];
+    blacklistedKernelModules = [ "kms" ];
+    loader = {
+      systemd-boot.enable = true;
+      timeout = 0;
+    };
+  };
+
+  # Pick only one of the below networking options.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Set your time zone.
+  time.timeZone = "America/Sao_Paulo";
+  time.hardwareClockInLocalTime = true;
+
+  networking = {
+    hostName = "William";
+    networkmanager.enable = true;
+
+    firewall = {
+      allowedTCPPorts = [
+        42000 # Warpinator
+        42001 # Warpinator
+      ];
+      allowedUDPPorts = [
+        42000 # Warpinator
+        42001 # Warpinator
+      ];
+    };
+  };
+
+  # Fonts
+  fonts = {
+    packages = with pkgs; [
+      nerdfonts
+      noto-fonts
+      noto-fonts-emoji
+      noto-fonts-color-emoji
+      noto-fonts-cjk-sans
+      noto-fonts-cjk-serif
+      google-fonts
+    ];
+  };
+
+  # Select internationalisation properties.
+  i18n = {
+    defaultLocale = "pt_BR.UTF-8"; # Erros, Warnings, Etc ...
+    extraLocaleSettings = {
+      LC_ADDRESS     = "pt_BR.UTF-8";
+      LC_MEASUREMENT = "pt_BR.UTF-8";
+      LC_MONETARY    = "pt_BR.UTF-8";
+      LC_NAME        = "pt_BR.UTF-8";
+      LC_NUMERIC     = "pt_BR.UTF-8";
+      LC_PAPER       = "pt_BR.UTF-8";
+      LC_TELEFONE    = "pt_BR.UTF-8";
+      LC_TIME        = "pt_BR.UTF-8";
+    };
+  };
+
+  console = {
+    font = "Lat2-Terminus32";
+    keyMap = "br-abnt2";
+    colors = [
+      # Use terminal.sexy with alacritty
+      # Material
+      "263238"
+      "ff9800"
+      "8bc34a"
+      "ffc107"
+      "03a9f4"
+      "e91e63"
+      "009688"
+      "cfd8dc"
+      "37474f"
+      "ffa74d"
+      "9ccc65"
+      "ffa000"
+      "81d4fa"
+      "ad1457"
+      "26a69a"
+      "eceff1"
+    ];
+  };
+
+  # Services
+  services = {
+    pipewire = {
+      enable = true;
+      audio = {
+        enable = true;
+      };
+      wireplumber = {
+        enable = true;
+      };
+      pulse = {
+        enable = true;
+      };
+      jack = {
+        enable = true;
+      };
+      alsa = {
+        enable = true;
+        support32Bit = true;
+      };
+    };
+    avahi = {
+      enable = true;
+      nssmdns = true;
+      openFirewall = true;
+    };
+    devmon = {
+      enable = true;
+    };
+    greetd = {
+      enable = true;
+      settings = {
+        default_session = {
+          command = "${pkgs.greetd.tuigreet}/bin/tuigreet --user-menu -t -c Hyprland";
+        };
+      };
+    };
+    openssh = {
+      enable = true;
+      settings = {
+        # Forbid root login through SSH.
+        PermitRootLogin = "no";
+        # Use keys only. Remove if you want to SSH using password (not recomended)
+        PasswordAuthentication = false;
+      };
+    };
+    picom = {
+      enable = true;
+    };
+    printing = {
+      enable = true;
+      drivers = with pkgs; [ epson-escpr ];
+      browsing = true;
+      defaultShared = true;
+    };
+    transmission = {
+      enable = true;
+      package = pkgs.transmission-gtk;
+    };
+    udisks2 = {
+      enable = true;
+    };
+    xserver = {
+      enable = true;
+      layout = "br";
+      excludePackages = with pkgs; [ xterm ];
+      videoDrivers = [ "nvidia" ];
+      displayManager.startx.enable = true;
+      windowManager.awesome.enable = true;
+    };
+  };
+
+  # Hardware
+  hardware = {
+    # Bluetooth
+    bluetooth = {
+      enable = true;
+    };
+
+    # Nvidia
+    nvidia = {
+      powerManagement.enable = true;
+      forceFullCompositionPipeline = true;
+      nvidiaSettings = true;
+      modesetting.enable = true;
+    };
+
+    # Opengl
+    opengl = {
+      enable = true;
+      driSupport = true;
+      driSupport32Bit = true;
+      extraPackages = with pkgs; [
+        nvidia-vaapi-driver
+      ];
+    };
+
+    # PulseAudio
+    pulseaudio = {
+      enable = false;
+    };
+
+    # Steam
+    steam-hardware.enable = true;
+
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users = {
+    defaultUserShell = pkgs.fish;
+    users = {
+      william = {
+        isNormalUser = true;
+        extraGroups = [ "wheel" "networkmanager" "libvirtd" ];
+      };
+    };
+  };
+
+  # Virtualisation
+  virtualisation.libvirtd = {
+    enable = true;
+    qemu.ovmf.enable = true;
+  };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment = {
+    defaultPackages = [];
+    systemPackages = with pkgs; [
+      #### Core
+      lld
+      gcc
+      glibc
+      clang
+      llvmPackages.bintools
+      wget
+      killall
+      zip
+      unzip
+      exfat
+      lm_sensors
+      git
+
+      libnotify
+      exiftool
+
+      #### Vulkan
+      vulkan-tools
+      vulkan-validation-layers
+      vulkan-loader
+      vulkan-headers
+
+      #### Linux
+      linuxHeaders
+
+      #### Party Tricks
+      cmatrix
+      cowsay
+      sl
+      lolcat
+      figlet
+
+      #### Browser
+      (google-chrome.override {
+        commandLineArgs = [
+          "--force-device-scale-factor=1.5"
+          "--enable-blink-features=MiddleClickAutoscroll"
+        ];
+      })
+      librewolf
+
+      #### Media
+      yt-dlp
+      cava
+      pavucontrol
+      zathura
+      lf
+      ffmpeg
+      ffmpegthumbnailer
+      mpv
+      cinnamon.warpinator
+      virt-manager
+
+      #### Programing
+        ### Languages
+          # Rust
+          rustup
+          # Python
+          python3
+
+          # Dependencies
+          rocmPackages.llvm.lldb
+
+          # Nix
+          nil
+
+          # Markdown
+          marksman
+          slides
+          graph-easy
+          glow
+
+          # Bash
+          nodePackages_latest.bash-language-server
+
+          # Editors
+          helix
+
+          # Tools
+          lazygit
+          tokei
+        
+      #### Proprietary
+      (discord.override {
+        withOpenASAR = true;
+        # withVencord = true;
+      })
+
+      starship
+      feh
+      bat
+      p7zip
+      freshfetch
+      neofetch
+      trash-cli
+      fzf
+      fd
+      ripgrep
+      btop
+      eza
+      kitty
+      speedtest-cli
+
+      # Hyprland
+      wlr-randr
+      rofi-wayland
+      swaybg
+      mpvpaper
+      imv
+      dunst
+      grim
+      slurp
+      wl-clipboard
+      wf-recorder
+      wlroots
+
+      # Awesome 
+      (__getFlake "github:fortuneteller2k/nixpkgs-f2k").packages.${system}.awesome-git
+
+      
+      ani-cli
+      mangal
+      mangohud
+      yuzu-mainline
+      cemu
+      rpcs3
+    ];
+  };
+
+  # Programs
+  programs = {
+    dconf = {
+      enable = true;
+    };
+    fish = {
+      enable = true;
+    };
+    gnupg = {
+      agent = {
+        enable = true;
+        enableSSHSupport = true;
+      };
+    };
+    xwayland = {
+      enable = true;
+    };
+    hyprland = {
+      enable = true;
+      xwayland = {
+        enable = true;
+      };
+      enableNvidiaPatches = true;
+    };
+    waybar = {
+      enable = true;
+    };
+    steam = {
+      enable = true;
+      remotePlay.openFirewall = true;
+      dedicatedServer.openFirewall = true;
+    };
+  };
+
+  # Xdg
+  xdg = {
+    portal = {
+      enable = true;
+      wlr.enable = true;
+    };
+    icons.enable = true;
+    mime = {
+      enable = true;
+      defaultApplications = {
+        "text/*" = "Helix.desktop";
+        "text/plain" = "Helix.desktop";
+        "application/pdf" = "org.pwmt.zathura.desktop";
+        "application/rdf+xml" = "org.pwmt.zathura.desktop";
+        "application/rss+xml" = "org.pwmt.zathura.desktop";
+        "application/xhtml+xml" = "org.pwmt.zathura.desktop";
+        "application/xhtml_xml" = "org.pwmt.zathura.desktop";
+        "application/xml" = "org.pwmt.zathura.desktop";
+        "image/*" = "imv.desktop";
+        "image/png" = "imv.desktop";
+        "image/jpeg" = "imv.desktop";
+        "image/gif" = "imv.desktop";
+        "image/webp" = "imv.desktop";
+        "video/*" = "mpv.desktop";
+        # "text/html" = "librewolf.desktop";
+        # "text/xml" = "librewolf.desktop";
+        # "x-scheme-handler/http" = "librewolf.desktop";
+        # "x-scheme-handler/https" = "librewolf.desktop";
+        # "x-scheme-handler/about" = "librewolf.desktop";
+        # "x-scheme-handler/unknown" = "librewolf.desktop";
+        # "x-scheme-handler/mailto" = "librewolf.desktop";
+        # "x-scheme-handler/webcal" = "librewolf.desktop";
+      };
+    };
+  };
+
+  # Security
+  security = {
+    sudo.enable = false;
+    rtkit.enable = true;
+    doas = {
+      enable = true;
+      extraRules = [{
+        groups = [ "wheel" ];
+        keepEnv = true;
+        persist = true;
+      }];
+    };
+  };
+
+  # Variables
+  environment.variables = {
+    XCURSOR_SIZE = "48";
+    
+    FZF_DEFAULT_COMMAND = "fd -H";
+
+    LIBSEAT_BACKEND = "logind";
+
+    WLR_NO_HARDWARE_CURSORS = "1";
+    WLR_RENDERER = "vulkan";
+    WLR_DRM_NO_ATOMIC = "1";
+
+    NIXOS_OZONE_WL = "1";
+
+    GDK_BACKEND = "wayland,x11";
+
+    QT_QPA_PLATFORM = "xcb;wayland-egl";
+    QT_SCALE_FACTOR = "1.5";
+    QT_AUTO_SCREEN_SCALE_FACTOR = "0";
+
+    KITTY_ENABLE_WAYLAND = "1";
+
+    LIBVA_DRIVER_NAME = "nvidia";
+    XDG_SESSION_TYPE = "wayland";
+    GBM_BACKEND = "nvidia-drm";
+    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+
+    EDITOR = "hx";
+    VISUAL = "hx";
+  };
+
+  nixpkgs = {
+    config = {
+      allowUnfree = true;
+    };
+    overlays = [ 
+      (__getFlake "github:fortuneteller2k/nixpkgs-f2k").overlays.default
+      (final: prev: {
+        discord = prev.discord.overrideAttrs(_: {
+          src = builtins.fetchTarball {
+            url = "https://discord.com/api/download?platform=linux&format=tar.gz";
+            sha256 = "0qzdvyyialvpiwi9mppbqvf2rvz1ps25mmygqqck0z9i2q01c1zd";
+          };
+        });
+      })
+    ];
+  };
+
+  nix = {
+    # This will add each flake input as a registry
+    # To make nix3 commands consistent with your flake
+    # registry = lib.mapAttrs (_: value: { flake = value; }) inputs;
+
+    # This will addicionally add your inputs to the system's legacy channels
+    # Making legacy nix commands consistent as well, awesome!
+    # nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
+
+    settings = {
+      # Enable flakes and new 'nix' command
+      experimental-features = "nix-command flakes";
+      # Deduplicate and optimize nix store
+      auto-optimise-store = true;
+    };
+
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 7d";
+    };
+  };
+
+  home-manager.users.william = { pkgs, ... }: {
+    home.stateVersion = "23.11";
+
+    gtk = {
+      enable = true;
+      cursorTheme = {
+        package = pkgs.bibata-cursors;
+        name = "Bibata-Modern-Ice";
+        size = 48;
+      };
+      theme = {
+        package = pkgs.graphite-gtk-theme;
+        name = "Graphite-Dark";
+      };
+      iconTheme = {
+        package = pkgs.tela-circle-icon-theme;
+        name = "Tela-circle";
+      };
+    };
+
+    qt = {
+      enable = true;
+      platformTheme = "gtk";
+      style.name = "Graphite-Dark";
+    };
+
+    home.pointerCursor = {
+      x11.enable = true;
+      package = pkgs.bibata-cursors;
+      name = "Bibata-Modern-Ice";
+      size = 48;
+      gtk.enable = true;
+    };
+  };
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  # system.copySystemConfiguration = true;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It's perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.11"; # Did you read the comment?
+
+}
+
