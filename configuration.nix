5dd405734d2e189de07760e8af2f3042ac3914af
William Da Silva Vieira <89865877+WilliamDaSilvaVieira@users.noreply.github.com>
2023-12-03 22:36:10 -0300

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -212,6 +212,8 @@
 
     # Steam
     steam-hardware.enable = true;
+
+    enableAllFirmware = true;
   };
 
   # Enable touchpad support (enabled default in most desktopManager).
@@ -582,7 +584,7 @@
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.11"; # Did you read the comment?
+  system.stateVersion = "24.05"; # Did you read the comment?
 
 }
 
